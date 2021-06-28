30f586ddf8aa8ab15459887e870bddc348f80676
tchartchke <chartai.kl@gmail.com>
2021-06-28 12:31:54 -0500

    commit tree 70525f6527dcb3ad22e9d06601671663a5edf507

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+// import { connect } from 'react-redux';
 
 export class App extends Component {
   render() {
@@ -13,4 +14,6 @@ export class App extends Component {
   }
 };
 
+// export default connect()(App);
 export default App;
+
